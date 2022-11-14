e74f2c52974253bcde3a338870bd7526bda89b7a
Kelley Fraser <kelleyirwinfraser@gmail.com>
2022-11-14 13:34:47 +0000

    Create component to hold hero and about blocks.

---
+++ src/components/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import AboutSection from './blocks/aboutBlock';
+import HeroSection from './blocks/heroBlock';
+import FootBar from './footBar';
+
+const Home = () => {
+  return (
+    <>
+      <HeroSection />
+      <AboutSection />
+      <FootBar />
+    </>
+  )
+}
+
+export default Home;
