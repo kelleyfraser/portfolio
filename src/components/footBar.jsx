9a4f976e04a4987e0a35b3acebeba6e82cdd6ad9
Kelley Fraser <kelleyirwinfraser@gmail.com>
2022-11-15 17:25:00 +0000

    Build footbar component.

---
+++ src/components/footBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FootBar = () => {
+  return (
+    <div className="footbar" id="footbar">
+      <p className="footbar-text">&copy; 2022 Kelley Fraser</p>
+    </div>
+  )
+}
+
+export default FootBar;
