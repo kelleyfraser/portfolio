3958aca12812bcf619399440e84906f48780f8fd
Kelley Fraser <kelleyirwinfraser@gmail.com>
2022-11-22 15:31:53 +0000

    Build contactForm component.

--- src/components/contactForm.jsx
+++ src/components/contactForm.jsx
@@ -1,11 +1,86 @@
-import React, { forwardRef } from 'react'
+import React, { useState, useEffect } from 'react'
+import Button from './common/button';
+import FootBar from './footBar';
 
-const ContactForm = (props, ref) => {
+const ContactForm = () => {
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  const [mailerState, setMailerState] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    message: ""
+  });
+
+  const handleStateChange = (e) => {
+    setMailerState((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const submitEmail = async (e) => {
+    e.preventDefault();
+    console.log({ mailerState });
+    const response = await fetch("http://localhost:5001/send", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ mailerState }),
+    })
+      .then((res) => res.json())
+      .then(async (res) => {
+        const resData = await res;
+        console.log(resData);
+        if (resData.status === "success") {
+          alert("Message Sent");
+        } else if (resData.status === "fail") {
+          alert("Message failed to send");
+        }
+      })
+      .then(() => {
+        setMailerState({
+          email: "",
+          firstName: "",
+          lastName: "",
+          message: "",
+        });
+      });
+  };
+
   return (
-    <section ref={ref} id="contact" className="block">
-      <div>Contact Form</div>
-    </section>
+    <>
+      <section className="contact">
+        <div id="contact" className="contact">
+          <div><h2>Contact</h2></div>
+          <form onSubmit={submitEmail}>
+            <div className="input-group half-width">
+              <label htmlFor="firstName">First Name</label>
+              <input onChange={handleStateChange} type="text" name="firstName" value={mailerState.firstName}></input>
+            </div>
+            <div className="input-group half-width">
+              <label htmlFor="lastName">Last Name</label>
+              <input onChange={handleStateChange} type="text" name="lastName" value={mailerState.lastName}></input>
+            </div>
+            <div className="input-group full-width">
+              <label htmlFor="email">Email</label>
+              <input onChange={handleStateChange} type="email" name="email" value={mailerState.email}></input>
+            </div>
+            <div className="input-group full-width">
+              <label htmlFor="message">Message</label>
+              <textarea onChange={handleStateChange} rows="6" name="message" value={mailerState.message}></textarea>
+            </div>
+            <Button type="submit" name="Submit" />
+          </form>
+        </div>
+      </section>
+      <FootBar />
+    </>
   )
 }
 
-export default forwardRef(ContactForm);
+export default ContactForm;
