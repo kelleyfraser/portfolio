37f80b981fb2b7f07b0dc7247218153af77410fb
Kelley Fraser <kelleyirwinfraser@gmail.com>
2022-11-22 16:24:30 +0000

    Build app component using react-router-dom.

--- src/App.js
+++ src/App.js
@@ -1,81 +1,34 @@
-import React, { useRef, useState, useEffect } from 'react';
-import AboutSection from './components/aboutSection';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import ContactForm from './components/contactForm';
 import ExperienceSection from './components/experienceSection';
-import HeroSection from './components/heroSection';
+import HomeSection from './components/homeSection';
 import NavBar from './components/navBar';
 import ProjectsSection from './components/projectsSection';
-import SkillsSection from './components/skillsSection';
+import EducationBlock from './components/blocks/educationBlock';
+import WorkBlock from './components/blocks/workBlock';
+import CoursesBlock from './components/blocks/coursesBlock';
+import NotFound from './components/notFound';
 import './App.scss';
 
-const getDimensions = ele => {
-  const { height } = ele.getBoundingClientRect();
-  const offsetTop = ele.offsetTop;
-  const offsetBottom = offsetTop + height;
-  
-  return {
-    height,
-    offsetTop,
-    offsetBottom
-  };
-};
-
 function App() {
-  const [visibleSection, setVisibleSection] = useState();
-
-  const navBarRef = useRef(null);
-  const homeRef = useRef(null);
-  const aboutRef = useRef(null);
-  const experienceRef = useRef(null);
-  const skillsRef = useRef(null);
-  const projectsRef = useRef(null);
-  const contactRef = useRef(null);
-
-  const sectionRefs = [
-    { section: "home", ref: homeRef },
-    { section: "about", ref: aboutRef },
-    { section: "experience", ref: experienceRef },
-    { section: "skills", ref: skillsRef },
-    { section: "projects", ref: projectsRef },
-    { section: "contact", ref: contactRef },
-  ];
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const { height: navBarHeight } = getDimensions(navBarRef.current);
-      const scrollPosition = window.scrollY + navBarHeight + 80;
-
-      const selected = sectionRefs.find(({ref}) => {
-        const ele = ref.current;
-        if (ele) {
-          const { offsetBottom, offsetTop } = getDimensions(ele);
-          return scrollPosition > offsetTop && scrollPosition < offsetBottom;
-        }
-      });
-
-      if (selected && selected.section !== visibleSection) {
-        setVisibleSection(selected.section);
-      } else if (!selected && visibleSection) {
-        setVisibleSection(undefined);
-      }
-    };
-
-    handleScroll();
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [visibleSection]);  
-
   return (
     <div className="app">
-      <NavBar currentLink={visibleSection} ref={navBarRef} />
-      <HeroSection ref={homeRef} />
-      <AboutSection ref={aboutRef} />
-      <ExperienceSection ref={experienceRef} />
-      <SkillsSection ref={skillsRef} />
-      <ProjectsSection ref={projectsRef} />
-      <ContactForm ref={contactRef} />
+      <NavBar />
+      <Routes>
+        <Route index element={<HomeSection />} />
+        <Route path="/" element={<HomeSection />} />
+        <Route path="/experience" element={<ExperienceSection />}>
+          <Route index element={<EducationBlock />} />
+          <Route path="education" element={<EducationBlock />} />
+          <Route path="work" element={<WorkBlock />} />
+          <Route path="courses" element={<CoursesBlock />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+        <Route path="/projects" element={<ProjectsSection />} />
+        <Route path="/contact" element={<ContactForm />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
